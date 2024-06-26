a4bfa085b82d50b2650c29407ff3b415a5108a87
subbaksh <subbaksh@cisco.com>
2024-06-26 02:28:20 +0100

    logo changes; adding synheart logo; placeholder company logo

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Container, Stack } from 'react-bootstrap'
 import './header.css'
-import logo from './logo.svg'
+import synheartLogo from './synheart-logo.png'
+import companyLogo from './company-logo.png'
 
 export default function header() {
   return (
     <Container className="title">
-    <Stack direction="horizontal" gap={2}>
-        <div className="card-label"> <img id="shLogo" alt="Logo" src={logo}></img></div>
-        <div className="p-2"><h3 id="titleText">Synthetic Heart</h3></div>
+    <Stack direction="horizontal" gap={1}>
+        <div className="synheart-logo"> <img height={40} id="shLogo" alt="Logo" src={synheartLogo}></img></div>
+        <div className="p-2 title"><h3 id="titleText">Synthetic Heart</h3></div>
+        <div className="ms-auto company-logo"> <img height={70} id="companyLogo" alt="Logo" src={companyLogo}></img></div>
     </Stack>
     <hr></hr>
     </Container>
